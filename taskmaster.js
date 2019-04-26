4917d874b1edec98ae084ac5a430282f42837e98
martinbugayong <martinbugayong@gmail.com>
2019-04-25 20:38:45 -0700

    add checkbox

--- taskmaster.js
+++ taskmaster.js
@@ -25,10 +25,16 @@ var render = function () {
     container.innerHTML = '' 
     for (i = 0; i < tasks.length; i++) { // forloop dynamically creating <li> that is typed
         var node = document.createElement("li"); 
+        /* var delBtn = document.createElement("button");
+            delBtn.innerHTML = "-";*/
+        var checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
         var titlenode = document.createTextNode(tasks[i].title + " | "); 
         var descnode = document.createTextNode(tasks[i].description); 
         node.appendChild(titlenode) 
-        node.appendChild(descnode) 
+        node.appendChild(descnode)
+        //node.appendChild(delBtn)
+        node.appendChild(checkbox)
         container.appendChild(node) 
     }
 }
