c4bc1af1c9460d614672d29cbbdf41083a4bc273
martinbugayong <martinbugayong@gmail.com>
2019-04-29 08:18:47 -0700

    Add ID

--- taskmaster.js
+++ taskmaster.js
@@ -9,6 +9,7 @@ var tasks = [
     {
         title: "Example Task Title",
         description: "This is the decription, Check the checkbox when the task is done",
+        // id: makeid(6)
     }
 ]
 
@@ -17,16 +18,18 @@ var render = function () {
     container.innerHTML = '' 
     for (i = 0; i < tasks.length; i++) { // forloop dynamically creating <li> that is typed
         var node = document.createElement("li"); 
-        /* var delBtn = document.createElement("button");
-            delBtn.innerHTML = "-";*/
+        var delBtn = document.createElement("button");
+            delBtn.innerHTML = "-";
         var checkbox = document.createElement("input");
             checkbox.type = "checkbox";
         var titlenode = document.createTextNode(tasks[i].title + " | "); 
         var descnode = document.createTextNode(tasks[i].description); 
+        var RANDOM_ID = makeid(6);
+        node.id = RANDOM_ID
         node.appendChild(checkbox)
+        node.appendChild(delBtn)
         node.appendChild(titlenode) 
         node.appendChild(descnode)
-        //node.appendChild(delBtn)
         container.appendChild(node) 
     }
 }
@@ -34,11 +37,28 @@ var render = function () {
 var addTask = function () {
     var obj = { 
         title: taskTitle.value, 
-        description: taskDesc.value 
+        description: taskDesc.value,
+        id: makeid(6)
     }
     tasks.push(obj) 
     render(); 
+    console.log(obj);
 }
+
+// var delTask = function () {
+//     var toBeDeleted = node.
+// }
+
+// Make ID function 
+var makeid = function (length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+console.log(makeid)
 
 // event Listiners
 addBtn.addEventListener("click", addTask)
